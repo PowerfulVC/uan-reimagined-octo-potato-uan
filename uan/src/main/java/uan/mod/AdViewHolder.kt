999e8f8eded64582bc83bec0d7f8ce52ae0cf0b7
Mikhail <40062327+Mikhail2328@users.noreply.github.com>
2022-06-29 20:48:52 +0300

    native ad config, native ad view holder for recycler view adapter

--- uan/src/main/java/uan/mod/AdViewHolder.kt
+++ uan/src/main/java/uan/mod/AdViewHolder.kt
@@ -3,11 +3,12 @@ package uan.mod
 import android.view.View
 import android.widget.FrameLayout
 import androidx.recyclerview.widget.RecyclerView
+import uan.mod.configs.UaNativeAd
 
 class AdViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     private val adHolder: FrameLayout = view.findViewById(R.id.nativeAdHolder)
 
-    fun bind(nativeADUnit: String) {
-        Ad.showNative(adHolder, nativeADUnit)
+    fun bind(nativeADUnit: String, nativeADConfig: UaNativeAd) {
+        Ad.showNative(adHolder, nativeADUnit, nativeADConfig)
     }
 }
