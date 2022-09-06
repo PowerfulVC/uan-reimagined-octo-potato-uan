8b847eeee18f5531f398ab3e5aa90a200af5ede4
PowerfulVC <106018746+PowerfulVC@users.noreply.github.com>
2022-09-06 19:19:25 +0300

    Update AdViewHolder.kt

--- uan/src/main/java/uan/mod/use/AdViewHolder.kt
+++ uan/src/main/java/uan/mod/use/AdViewHolder.kt
@@ -3,7 +3,9 @@ package uan.mod.use
 import android.view.View
 import android.widget.FrameLayout
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.CoroutineScope
 import uan.mod.Ad
+import uan.mod.AdNew
 import uan.mod.R
 import uan.mod.configs.UaNativeAd
 
@@ -11,6 +13,10 @@ class AdViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     private val adHolder: FrameLayout = view.findViewById(R.id.nativeAdHolder)
 
     fun bind(nativeADUnit: String, nativeADConfig: UaNativeAd) {
+        Ad.showNative(adHolder, nativeADUnit, nativeADConfig)
+    }
+
+    fun bindNew(nativeADUnit: String, nativeADConfig: UaNativeAd, scope : CoroutineScope, uan : AdNew) {
         Ad.showNative(adHolder, nativeADUnit, nativeADConfig)
     }
 }
