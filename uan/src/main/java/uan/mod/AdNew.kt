461c991709478c42954780c03e4d315843300242
PowerfulVC <106018746+PowerfulVC@users.noreply.github.com>
2022-09-06 21:03:26 +0300

    Update AdNew.kt

--- uan/src/main/java/uan/mod/AdNew.kt
+++ uan/src/main/java/uan/mod/AdNew.kt
@@ -2,6 +2,7 @@ package uan.mod
 
 import android.app.Activity
 import android.widget.FrameLayout
+import uan.mod.callbacks.OnReInit
 import uan.mod.helper.AdUnitsHelper
 import uan.mod.helper.FrameAds
 
@@ -9,6 +10,7 @@ interface AdNew {
     val frameAds : FrameAds
     val adUnitsHelper : AdUnitsHelper
     suspend fun showSplashInter(activity: Activity, onAdClosed: () -> Unit)
+    suspend fun setupGlobalInitListener(callback : OnReInit)
     suspend fun showInter(activity: Activity, onAdClosed: () -> Unit)
     suspend fun showReward(activity: Activity, onRewardClosed: (rewarded: Boolean) -> Unit)
     suspend fun showAdInFrame(activity: Activity, frameLayout: FrameLayout)
